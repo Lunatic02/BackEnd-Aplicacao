c738ac22ad547ce54246e1b42a2632493268df85
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-09-04 19:29:33 -0300

    dotenv, e .Env.example criados

--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,20 @@ import cors from '@fastify/cors'
 import cookie from '@fastify/cookie'
 import { hiRoutes } from './routes/hiRoutes';
 import fastifyJwt from '@fastify/jwt';
-
+import dotenv from 'dotenv';
 
 export const app = fastify()
+dotenv.config();
+
+
 
 app.register(cookie)
 app.register(cors, {
   methods: ['GET', 'POST']
 })
+const secret = process.env.JWT_SECRET;
 app.register(fastifyJwt, {
-  secret: 'asdasdasd'
+  secret: `${secret}`
 })
 
 
